d6dc9cde559cd52e868a5e1680ddd15bf3619a4a
fabiosachetto <fabio@sachetto.com.br>
2024-12-11 19:23:03 -0300

    alterando cor do bg no menu e adicionei hover los links

--- roteiro-paraty/src/components/Navbar/Navbar.tsx
+++ roteiro-paraty/src/components/Navbar/Navbar.tsx
@@ -31,7 +31,7 @@ export const Navbar = () => {
   // };
 
   return (
-    <nav className="bg-gray-800 text-white p-4 sm:p-6 md:flex md:justify-between md:items-center">
+    <nav className="bg-blue-500 text-white p-4 sm:p-6 md:flex md:justify-between md:items-center">
       <div className="container mx-auto flex justify-between items-center">
 
         <Link href="/">
@@ -45,46 +45,46 @@ export const Navbar = () => {
           </div>
         </Link>
 
-        <div className={isOpen ? "flex absolute top-28 bg-gray-800 w-full p-4 left-0 gap-4 flex-col" : "hidden md:flex"}>
+        <div className={isOpen ? "flex absolute top-28 bg-blue-500 w-full p-4 left-0 gap-4 flex-col" : "hidden md:flex"}>
           {/* <div className={getMenuClasses()}> */}
-          <Link href="/" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Home
           </Link>
-          <Link href="/escuna" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/escuna" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Escunas
           </Link>
-          <Link href="/jeep" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/jeep" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Jeep
           </Link>
-          <Link href="/van" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/van" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Van
           </Link>
-          <Link href="/lancha" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/lancha" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Lancha
           </Link>
-          <Link href="/traineira" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/traineira" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Traineira
           </Link>
-          <Link href="/veleiro" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/veleiro" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Veleiro
           </Link>
-          <Link href="/personalizado" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/personalizado" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Roteiros Personalizados
           </Link>
-          <Link href="/quemsomos" className="mx-2 hover:text-gray-300" onClick={toggleMenu}>
+          <Link href="/quemsomos" className="mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800" onClick={toggleMenu}>
             Quem Somos Nós
           </Link>
-          <Link href="https://wa.me/+5524998793280" target="_blank" className="flex items-center mx-2 hover:text-gray-300">
+          <Link href="https://wa.me/+5524998793280" target="_blank" className="flex items-center mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800">
             <WhatsappIcon className="mr-2 w-5 h-5" />
-            Contate-nos e crie seu próprio roteiro!
+            Contate-nos!
           </Link>
-          <Link href="https://instagram.com/seuroteiroemparaty" target="_blank" className="flex items-center mx-2 hover:text-gray-300">
+          <Link href="https://instagram.com/seuroteiroemparaty" target="_blank" className="flex items-center mx-1 transition delay-150 duration-300 ease-in-out font-bold py-1 px-2 rounded-lg cursor-pointer bg-blue-500 hover:bg-blue-800">
             <InstagramIcon className="mr-2 w-5 h-5" />
             Siga-nos no Instagram!
           </Link>
         </div>
 
-        <div className="md:hidden flex items-center">
+        <div className="md:hidden flex items-center mr-2">
           <button onClick={() => {
             setIsOpen(!isOpen);
           }}
@@ -94,7 +94,8 @@ export const Navbar = () => {
               {isOpen ? (
                 <path fill="#ffffff" d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z" />
               ) : (
-                <><path fill="#ffffff" d="M441.13,166.52h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" /><path fill="#ffffff" d="M441.13,279.72h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" /><path fill="#ffffff" d="M441.13,392.92h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" /></>
+                <g fill="#ffffff"><path d="M417.4,224H94.6C77.7,224,64,238.3,64,256c0,17.7,13.7,32,30.6,32h322.8c16.9,0,30.6-14.3,30.6-32   C448,238.3,434.3,224,417.4,224z" /><path d="M417.4,96H94.6C77.7,96,64,110.3,64,128c0,17.7,13.7,32,30.6,32h322.8c16.9,0,30.6-14.3,30.6-32   C448,110.3,434.3,96,417.4,96z" /><path d="M417.4,352H94.6C77.7,352,64,366.3,64,384c0,17.7,13.7,32,30.6,32h322.8c16.9,0,30.6-14.3,30.6-32   C448,366.3,434.3,352,417.4,352z" /></g>
+                // <><path fill="#ffffff" d="M441.13,166.52h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" /><path fill="#ffffff" d="M441.13,279.72h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" /><path fill="#ffffff" d="M441.13,392.92h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" /></>
               )}
             </svg>
           </button>
