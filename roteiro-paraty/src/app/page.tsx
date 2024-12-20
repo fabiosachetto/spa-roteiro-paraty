83a55c74377f8d8f07f94cf38250a639a3830185
fabiosachetto <fabio@sachetto.com.br>
2024-12-19 21:48:12 -0300

    buscando informações da home do json

--- roteiro-paraty/src/app/page.tsx
+++ roteiro-paraty/src/app/page.tsx
@@ -1,208 +1,112 @@
+import infos from "@/data/infos-home.json";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const escunaInfo = infos.find(info => info.titulo_pagina === "Escunas");
+  const jeepInfo = infos.find(info => info.titulo_pagina === "Jeep");
+  const privativosInfo = infos.find(info => info.titulo_pagina === "Privativos");
+
   return (
     <div className="text-center">
+      {escunaInfo && (
+        <div key={escunaInfo.titulo_pagina}>
+          <section id="escuna" className="mt-7 p-7 text-white text-center">
+            <h2 className="text-4xl mb-5 uppercase">{escunaInfo.titulo_pagina}</h2>
+            <div className="inline-block">
+              <Image
+                src={`/assets/images/${escunaInfo.imagem}`}
+                alt={escunaInfo.alt_title}
+                title={escunaInfo.alt_title}
+                width={500}
+                height={333}
+                className="hidden md:block object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
+              />
+              <Image
+                src={`/assets/images/${escunaInfo.imagem}`}
+                alt={escunaInfo.alt_title}
+                title={escunaInfo.alt_title}
+                width={300}
+                height={200}
+                className="block md:hidden object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
+              />
+            </div>
 
-      <section id="escuna" className="mt-7 p-7 text-white text-center">
-
-        <h2 className="text-4xl mb-5 uppercase">Escunas</h2>
-
-        <div className="inline-block">
-          <Image
-            src="/assets/images/escuna.jpeg"
-            alt="Conheça Nossos Roteiros de Escunas"
-            title="Conheça Nossos Roteiros de Escunas"
-            width={500}
-            height={333}
-            className="hidden md:block object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-          <Image
-            src="/assets/images/escuna.jpeg"
-            alt="Conheça Nossos Roteiros de Escunas"
-            title="Conheça Nossos Roteiros de Escunas"
-            width={300}
-            height={200}
-            className="block md:hidden object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
+            <div className="mt-4">
+              <Link href="/escunas" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
+                Saiba Mais
+              </Link>
+            </div>
+          </section>
         </div>
+      )}
 
-        <div className="mt-4">
-          <Link href="/escunas" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
+      {jeepInfo && (
+        <div key={jeepInfo.titulo_pagina}>
+          <section id="jeep" className="mt-7 p-7 text-white text-center">
+            <h2 className="text-4xl mb-5 uppercase">{jeepInfo.titulo_pagina}</h2>
+            <div className="inline-block">
+              <Image
+                src={`/assets/images/${jeepInfo.imagem}`}
+                alt={jeepInfo.alt_title}
+                title={jeepInfo.alt_title}
+                width={500}
+                height={333}
+                className="hidden md:block object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
+              />
+              <Image
+                src={`/assets/images/${jeepInfo.imagem}`}
+                alt={jeepInfo.alt_title}
+                title={jeepInfo.alt_title}
+                width={300}
+                height={200}
+                className="block md:hidden object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
+              />
+            </div>
+
+            <div className="mt-4">
+              <Link href="/jeep" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
+                Saiba Mais
+              </Link>
+            </div>
+          </section>
         </div>
-      </section>
+      )}
 
-      <section id="jeep" className="mt-7 p-7 text-white">
-        <h2 className="text-4xl mb-5 uppercase">Jeep</h2>
-        <div className="inline-block">
-          <Image
-            src="/assets/images/jeep.jpeg"
-            alt="Conheça Nossos Roteiros de Jeep"
-            title="Conheça Nossos Roteiros de Jeep"
-            width={500}
-            height={333}
-            className="hidden md:block object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-          <Image
-            src="/assets/images/jeep.jpeg"
-            alt="Conheça Nossos Roteiros de Jeep"
-            title="Conheça Nossos Roteiros de Jeep"
-            width={300}
-            height={200}
-            className="block md:hidden object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
+      {privativosInfo && (
+        <div key={privativosInfo.titulo_pagina}>
+          <section id="privativos" className="mt-7 p-7 text-white text-center">
+            <h2 className="text-4xl mb-5 uppercase">{privativosInfo.titulo_pagina}</h2>
+            <h3 className="text-2xl mb-5 uppercase font-bold">
+              {privativosInfo.titulo}
+            </h3>
+            <div className="inline-block">
+              <Image
+                src={`/assets/images/${privativosInfo.imagem}`}
+                alt={privativosInfo.alt_title}
+                title={privativosInfo.alt_title}
+                width={500}
+                height={333}
+                className="hidden md:block object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
+              />
+              <Image
+                src={`/assets/images/${privativosInfo.imagem}`}
+                alt={privativosInfo.alt_title}
+                title={privativosInfo.alt_title}
+                width={300}
+                height={200}
+                className="block md:hidden object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
+              />
+            </div>
+
+            <div className="mt-4">
+              <Link href="/privativos" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
+                Saiba Mais
+              </Link>
+            </div>
+          </section>
         </div>
-
-        <div className="mt-4">
-          <Link href="/jeep" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
-        </div>
-      </section>
-
-      <section id="privativos" className="mt-7 p-7 text-white">
-        <h2 className="text-4xl mb-5 uppercase">Privativos</h2>
-
-        <h3 className="text-2xl mb-5 uppercase font-bold">
-          Lanchas, Veleiros e Mais
-        </h3>
-
-        <div className="inline-block">
-          <Image
-            src="/assets/images/lancha.png"
-            alt="Conheça Nossos Roteiros Privativos"
-            title="Conheça Nossos Roteiros Privativos"
-            width={500}
-            height={376}
-            className="hidden md:block object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-          <Image
-            src="/assets/images/lancha.png"
-            alt="Conheça Nossos Roteiros Privativos"
-            title="Conheça Nossos Roteiros Privativos"
-            width={300}
-            height={226}
-            className="block md:hidden object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-        </div>
-
-        <div className="mt-4">
-          <Link href="/privativos" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
-        </div>
-      </section>
-
-      {/* <section id="van" className="mt-7 p-7 text-white">
-        <h2 className="text-4xl mb-5 uppercase">Van (Trindade)</h2>
-        <div className="inline-block">
-          <Image
-            src="/assets/images/van.jpg"
-            alt="Conheça Nossos Roteiros de Van"
-            title="Conheça Nossos Roteiros de Van"
-            width={300}
-            height={400}
-            className="object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-        </div>
-
-        <div className="mt-4">
-          <Link href="/van" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
-        </div>
-      </section> 
-      
-      <section id="lancha" className="mt-7 p-7 text-white">
-        <h2 className="text-4xl mb-5 uppercase">Lancha (Privativa)</h2>
-        <div className="inline-block">
-          <Image
-            src="/assets/images/lancha.png"
-            alt="Conheça Nossos Roteiros de Lancha"
-            title="Conheça Nossos Roteiros de Lancha"
-            width={300}
-            height={226}
-            className="object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-        </div>
-
-        <div className="mt-4">
-          <Link href="/lancha" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
-        </div>
-      </section>
-
-      <section id="traineira" className="mt-7 p-7 text-white">
-        <h2 className="text-4xl mb-5 uppercase">Traineira (Privativa)</h2>
-        <div className="inline-block">
-          <Image
-            src="/assets/images/traineira.jpeg"
-            alt="Conheça Nossos Roteiros de Traineira"
-            title="Conheça Nossos Roteiros de Traineira"
-            width={300}
-            height={400}
-            className="object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-        </div>
-
-        <div className="mt-4">
-          <Link href="/traineira" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
-        </div>
-      </section>
-
-      <section id="veleiro" className="mt-7 p-7 text-white">
-        <h2 className="text-4xl mb-5 uppercase">Veleiro (Privativo)</h2>
-        <div className="inline-block">
-          <Image
-            src="/assets/images/veleiro.png"
-            alt="Conheça Nossos Roteiros de Veleiro"
-            title="Conheça Nossos Roteiros de Veleiro"
-            width={300}
-            height={214}
-            className="object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-        </div>
-
-        <div className="mt-4">
-          <Link href="/veleiro" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
-        </div>
-      </section>
-
-      <section id="programas" className="mt-7 p-7 text-white bg-amber-950">
-        <h2 className="text-4xl mb-5 uppercase">Programas Personalizados</h2>
-        <div className="inline-block">
-          <Image
-            src="/assets/images/personalizado.png"
-            alt="Conheça Nossos Roteiros de Passeios Personalizados"
-            title="Conheça Nossos Roteiros de Passeios Personalizados"
-            width={300}
-            height={303}
-            className="object-cover transition duration-500 hover:scale-105 rounded-lg border-4 border-white"
-          />
-        </div>
-
-        <div className="mt-4">
-          <Link href="/personalizado" className="transition delay-150 duration-300 ease-in-out uppercase font-bold py-2 px-12 rounded-full cursor-pointer bg-[#0CC0DF] hover:bg-blue-500">
-            Saiba Mais
-          </Link>
-        </div>
-      </section>
-
-      {/* <section id="quemsomosnos" className="mt-7 p-7 bg-lime-200">
-        <h2 className="text-4xl uppercase">Quem Somos Nós</h2>
-        <p>
-          Somos uma agência online criada para fazer você ter momentos incríveis na cidade de Paraty. Entre em contato conosco, tire suas dúvidas, compartilhe suas expectativas e te ajudaremos a encontrar o passeio perfeito para você.
-        </p>
-      </section> */}
+      )}
     </div>
   );
-};
-
+}
