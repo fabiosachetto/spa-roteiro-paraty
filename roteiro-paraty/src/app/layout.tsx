0e453a66765f191c9aaa193fc78887bb69cb6991
fabiosachetto <fabio@sachetto.com.br>
2024-12-22 20:58:22 -0300

    google analytics dando pau

--- roteiro-paraty/src/app/layout.tsx
+++ roteiro-paraty/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "@/components/Footer/Footer";
 import { GoogleAnalytics } from "@/components/GoogleAnalytics/GoogleAnalytics";
 import { Navbar } from "@/components/Navbar/Navbar";
-// import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -33,13 +33,7 @@ export default function RootLayout({
 
           <Footer />
         </main>
-        {/* <Analytics /> */}
-        {/* <!-- Google Tag Manager (noscript) -->
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBHXDD3C"
-            height="0" width="0" className="hidden invisible"></iframe>
-        </noscript>
-        <!-- End Google Tag Manager (noscript) --> */}
+        <Analytics />        
       </body>
     </html>
   );
