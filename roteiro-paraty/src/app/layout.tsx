b8a44045b09c546085acdb65d2fdb86d8a7fa557
fabiosachetto <fabio@sachetto.com.br>
2024-12-22 14:32:31 -0300

    arrumando google analytics do painel da vercel

--- roteiro-paraty/src/app/layout.tsx
+++ roteiro-paraty/src/app/layout.tsx
@@ -1,18 +1,10 @@
 import { Footer } from "@/components/Footer/Footer";
 import { GoogleAnalytics } from "@/components/GoogleAnalytics/GoogleAnalytics";
 import { Navbar } from "@/components/Navbar/Navbar";
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
-// <!-- Google Tag Manager -->
-{/* <script>
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TBHXDD3C');</script> */}
-// <!-- End Google Tag Manager -->
 
 const openSans = localFont({
   src: "./fonts/OpenSans-Regular.ttf",
@@ -41,12 +33,13 @@ export default function RootLayout({
 
           <Footer />
         </main>
-        {/* <!-- Google Tag Manager (noscript) --> */}
+        <Analytics />
+        {/* <!-- Google Tag Manager (noscript) -->
         <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBHXDD3C"
             height="0" width="0" className="hidden invisible"></iframe>
         </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
+        <!-- End Google Tag Manager (noscript) --> */}
       </body>
     </html>
   );
