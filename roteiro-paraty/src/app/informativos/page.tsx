96f59af7cd8297faea98bbea7d84afdd3fb72bb1
verdi <fabio@sachetto.com.br>
2025-05-20 20:19:28 -0300

    arrumando estilo para o cliente aprovar.

--- roteiro-paraty/src/app/informativos/page.tsx
+++ roteiro-paraty/src/app/informativos/page.tsx
@@ -4,13 +4,15 @@ export default function Informativos() {
       <h2 className="mb-5 text-4xl uppercase">Informativos</h2>
 
       <div>
-        <h3>Meu Blog Estático</h3>
-        <p>Bem-vindo ao meu blog gerado com Next.js!</p>
+        <h3 className="font-bold mb-5">Meu Blog Estático</h3>
+        <p className="font-semibold mb-5">
+          Bem-vindo ao meu blog gerado com Next.js!
+        </p>
 
         <ul>
           <li>
-            <h4>Título do Post</h4>
-            <p>Publicado em: Data do Post</p>
+            <h4 className="font-semibold mb-5">Título do Post</h4>
+            <p className="font-extralight mb-5">Publicado em: Data do Post</p>
             <p>
               Conteúdo do Post Conteúdo do Post Conteúdo do Post Conteúdo do
               Post Conteúdo do Post Conteúdo do Post Conteúdo do Post Conteúdo
