87586d338387b1c8ba3d95f66758932fcb4eca11
verdi <fabio@sachetto.com.br>
2025-05-20 17:56:57 -0300

    subindo tudo desse novo branch

---
+++ roteiro-paraty/src/app/informativos/page.tsx
@@ -0,0 +1,36 @@
+export default function Informativos() {
+  return (
+    <section id="escuna" className="mt-7 p-7 text-center">
+      <h2 className="mb-5 text-4xl uppercase">Informativos</h2>
+
+      <div>
+        <h3>Meu Blog Estático</h3>
+        <p>Bem-vindo ao meu blog gerado com Next.js!</p>
+
+        <ul>
+          <li>
+            <h4>Título do Post</h4>
+            <p>Publicado em: Data do Post</p>
+            <p>
+              Conteúdo do Post Conteúdo do Post Conteúdo do Post Conteúdo do
+              Post Conteúdo do Post Conteúdo do Post Conteúdo do Post Conteúdo
+              do Post Conteúdo do Post Conteúdo do Post Conteúdo do Post
+              Conteúdo do PostConteúdo do Post Conteúdo do Post Conteúdo do Post
+              Conteúdo do Post Conteúdo do Post Conteúdo do PostConteúdo do Post
+              Conteúdo do Post Conteúdo do Post Conteúdo do Post Conteúdo do
+              Post Conteúdo do PostConteúdo do Post Conteúdo do Post Conteúdo do
+              Post Conteúdo do Post Conteúdo do Post Conteúdo do PostConteúdo do
+              Post Conteúdo do Post Conteúdo do Post Conteúdo do Post Conteúdo
+              do Post Conteúdo do PostConteúdo do Post Conteúdo do Post Conteúdo
+              do Post Conteúdo do Post Conteúdo do Post Conteúdo do PostConteúdo
+              do Post Conteúdo do Post Conteúdo do Post Conteúdo do Post
+              Conteúdo do Post Conteúdo do PostConteúdo do Post Conteúdo do Post
+              Conteúdo do Post Conteúdo do Post Conteúdo do Post Conteúdo do
+              Post
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
--- roteiro-paraty/src/components/Footer/Footer.tsx
+++ roteiro-paraty/src/components/Footer/Footer.tsx
@@ -1,11 +1,25 @@
 // import Image from "next/image";
-import { AmexIcon, BoletoIcon, DinersIcon, EloIcon, EmailIcon, HipercardIcon, InstagramIcon, MastercardIcon, PixIcon, VisaIcon, WhatsappIcon } from "@/components/Icons/Icons";
+import {
+  AmexIcon,
+  BoletoIcon,
+  DinersIcon,
+  EloIcon,
+  EmailIcon,
+  HipercardIcon,
+  InstagramIcon,
+  MastercardIcon,
+  PixIcon,
+  VisaIcon,
+  WhatsappIcon,
+} from "@/components/Icons/Icons";
 import Image from "next/image";
 import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer id="contato" className="w-full mt-7 p-7 bg-black bg-opacity-50 overflow-auto ">
+    <footer
+      id="contato"
+      className="w-full mt-7 p-7 bg-black bg-opacity-50 overflow-auto ">
       <div className="overflow-auto max-w-md m-auto">
         <h2 className="mb-5 text-4xl uppercase text-white text-center">
           Seu Roteiro em Paraty
@@ -13,19 +27,36 @@ export const Footer = () => {
 
         <ol className="text-white justify-center grid">
           <li className="mb-4 hover:text-black py-1 px-3 rounded-full cursor-pointer hover:bg-[#0CC0DF]">
-            <Link href="https://instagram.com/seuroteiroemparaty" target="_blank" className="flex items-center">
+            <Link
+              href="/informativos"
+              className="flex items-center">
+              <InstagramIcon className="mr-2 w-5 h-5" />
+              Informativos
+            </Link>
+          </li>
+          <li className="mb-4 hover:text-black py-1 px-3 rounded-full cursor-pointer hover:bg-[#0CC0DF]">
+            <Link
+              href="https://instagram.com/seuroteiroemparaty"
+              target="_blank"
+              className="flex items-center">
               <InstagramIcon className="mr-2 w-5 h-5" />
               Instagram
             </Link>
           </li>
           <li className="mb-4 hover:text-black py-1 px-3 rounded-full cursor-pointer hover:bg-[#0CC0DF]">
-            <Link href="https://api.whatsapp.com/send?phone=5524992779411&text=Vim%20do%20Site%20e%20cliquei%20em%20WhatsApp%20no%20Rodapé." target="_blank" className="flex items-center">
+            <Link
+              href="https://api.whatsapp.com/send?phone=5524992779411&text=Vim%20do%20Site%20e%20cliquei%20em%20WhatsApp%20no%20Rodapé."
+              target="_blank"
+              className="flex items-center">
               <WhatsappIcon className="mr-2 w-5 h-5" />
               WhatsApp
             </Link>
           </li>
           <li className="mb-4 hover:text-black pb-0 text-center py-1 px-3 rounded-full cursor-pointer hover:bg-[#0CC0DF]">
-            <Link href="mailto:seuroteiroemparaty@gmail.com" target="_blank" className="inline-flex items-center">
+            <Link
+              href="mailto:seuroteiroemparaty@gmail.com"
+              target="_blank"
+              className="inline-flex items-center">
               <EmailIcon className="mr-2 w-5 h-5" />
               E-Mail
             </Link>
@@ -81,13 +112,17 @@ export const Footer = () => {
       </div>
 
       <div className="overflow-auto max-w-md m-auto text-center pt-4 text-white border-t border-double border-t-white">
-        <Link href="/politica-de-privacidade" className="inline-block hover:text-black py-1 px-3 rounded-full cursor-pointer hover:bg-[#0CC0DF]">
+        <Link
+          href="/politica-de-privacidade"
+          className="inline-block hover:text-black py-1 px-3 rounded-full cursor-pointer hover:bg-[#0CC0DF]">
           Política de Privacidade.
         </Link>
       </div>
 
       <div className="overflow-auto max-w-md m-auto text-center mt-4 pt-4 text-white border-t border-double border-t-white">
-        <Link href="https://cadastur.turismo.gov.br/cadastur/#!/public/qrcode/47072440000118" target="_blank">
+        <Link
+          href="https://cadastur.turismo.gov.br/cadastur/#!/public/qrcode/47072440000118"
+          target="_blank">
           <div>
             <Image
               src="/assets/images/cadastur.png"
@@ -101,8 +136,11 @@ export const Footer = () => {
       </div>
 
       <div className="overflow-auto max-w-md m-auto text-center mt-4 pt-4 text-white border-t border-double border-t-white">
-
-        <Link id="link-brain-tech" href="https://api.whatsapp.com/send?phone=5524992681490&text=Vim%20do%20Site%20Seu%20Roteiro%20em%20Paraty%20e%20gostaria%20de%20fazer%20um%20orçamento." target="_blank" className="hover:animate-bounce inline-block text-white px-3 rounded">
+        <Link
+          id="link-brain-tech"
+          href="https://api.whatsapp.com/send?phone=5524992681490&text=Vim%20do%20Site%20Seu%20Roteiro%20em%20Paraty%20e%20gostaria%20de%20fazer%20um%20orçamento."
+          target="_blank"
+          className="hover:animate-bounce inline-block text-white px-3 rounded">
           Desenvolvido por
           <Image
             src="/assets/images/logo-brain-tech.png"
@@ -113,8 +151,7 @@ export const Footer = () => {
           />
           Brain Tech
         </Link>
-
       </div>
     </footer>
-  )
-}
+  );
+};
